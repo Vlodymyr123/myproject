c6e9dfc123f7d78ff1eac2622d4f32d095a360c6
Vlodymyr123 <enerzz20@gmail.com>
2019-10-29 22:47:43 +0200

    laba8

---
+++ Laba8_task1/Laba8_task1/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba8_task1
+{
+    class Program
+    {
+        class Garage
+        {
+            List<Car> listOfCars = new List<Car>();
+
+            public void Create_new_car()
+            {
+                string name;
+                string color;
+                int speed;
+                int year;
+
+                Console.Write("Enter a name of car: ");
+                name = Console.ReadLine();
+
+                Console.Write("Enter a color of car: ");
+                color = Console.ReadLine();
+
+                Console.Write("Enter car speed: ");
+                while (!int.TryParse(Console.ReadLine(), out speed) || speed <= 0)
+                {
+                    Console.Write("Enter a correct speed value, please: ");
+                }
+
+                Console.Write("Enter a cars year of issue: ");
+                while (!int.TryParse(Console.ReadLine(), out year) || year < 1900)
+                {
+                    Console.Write("Enter a correct year, please: ");
+                }
+
+                Car newCar = new Car(name, color, speed, year);
+
+                listOfCars.Add(newCar);
+            }
+
+            public void Display(string name)
+            {
+                foreach (Car i in listOfCars)
+                {
+                    if (i.Name == name)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3}", i.Name, i.Color, i.Speed, i.Year_of_issue);
+                    }
+                }
+            }
+
+            public void DisplayByColor(string color)
+            {
+                foreach (Car i in listOfCars)
+                {
+                    if (i.Color == color)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3}", i.Name, i.Color, i.Speed, i.Year_of_issue);
+                    }
+                }
+            }
+
+            public void Display(int speed)
+            {
+                foreach (Car i in listOfCars)
+                {
+                    if (i.Speed == speed)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3}", i.Name, i.Color, i.Speed, i.Year_of_issue);
+                    }
+                }
+            }
+
+            public void DisplayByYear(int year)
+            {
+                foreach (Car i in listOfCars)
+                {
+                    if (i.Year_of_issue == year)
+                    {
+                        Console.WriteLine("{0} - {1} - {2} - {3}", i.Name, i.Color, i.Speed, i.Year_of_issue);
+                    }
+                }
+            }
+
+            public void Show_all_cars()
+            {
+                foreach(Car all_cars in listOfCars)
+                {
+                    Console.WriteLine("{0} - {1} - {2} - {3}", all_cars.Name, all_cars.Color, all_cars.Speed, all_cars.Year_of_issue);
+                }
+            }
+            public void Delete_car(int position)
+            {
+                listOfCars.RemoveAt(position);
+            }
+        }
+        class Car
+        {
+            public string Name { get; }
+            public string Color { get; }
+            public int Speed { get; }
+            public int Year_of_issue { get; }
+
+            public Car() { }
+            public Car(string name, string color, int speed, int year_of_issue)
+            {
+                Name = name;
+
+                Color = color;
+
+                Speed = speed;
+
+                Year_of_issue = year_of_issue;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Garage myGarage = new Garage();
+
+            Console.WriteLine("1 - add new car");
+            Console.WriteLine("2 - delete car from selected position");
+            Console.WriteLine("3 - show cars by one characteristic");
+            Console.WriteLine("4 - show all cars");
+            Console.WriteLine("5 - for exit");
+            int way;
+
+            do
+            {
+                Console.Write("Select way:");
+
+                way = Convert.ToInt32(Console.ReadLine());
+                switch (way)
+                {
+                    case 1:
+                        {
+                            myGarage.Create_new_car();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Choose position that you want to delete: ");
+                            int position = Convert.ToInt32(Console.ReadLine());
+                            myGarage.Delete_car(position);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("1 - name");
+                            Console.WriteLine("2 - color");
+                            Console.WriteLine("3 - speed");
+                            Console.WriteLine("4 - year of issue");
+                            Console.WriteLine("5 - don`t choose");
+                            Console.Write("Choose which characteristic you need:");
+                            int characteristic = Convert.ToInt32(Console.ReadLine());
+                            switch (characteristic)
+                            {
+                                case 1:
+                                    {
+                                        Console.Write("Input name: ");
+                                        string name = Console.ReadLine();
+                                        myGarage.Display(name);
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Console.Write("Input color: ");
+                                        string color = Console.ReadLine();
+                                        myGarage.DisplayByColor(color);
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        Console.Write("Input speed: ");
+                                        int speed;
+                                        Console.Write("Enter car speed: ");
+                                        while (!int.TryParse(Console.ReadLine(), out speed) || speed <= 0)
+                                        {
+                                            Console.Write("Enter a correct speed value, please: ");
+                                        }
+                                        myGarage.Display(speed);
+                                        break;
+                                    }
+                                case 4:
+                                    {
+                                        Console.Write("Input year: ");
+                                        int year;
+                                        Console.Write("Enter a cars year of issue: ");
+                                        while (!int.TryParse(Console.ReadLine(), out year) || year < 1900)
+                                        {
+                                            Console.Write("Enter a correct year, please: ");
+                                        }
+                                        myGarage.DisplayByYear(year);
+                                        break;
+                                    }
+                                case 5:
+                                    {
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            myGarage.Show_all_cars();
+                            break;
+                        }
+                    case 5:
+                        {
+                            break;
+                        }
+                }
+            } while (way != 5);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba8_task1/Laba8_task2/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba8_task2
+{
+    class Program
+    {
+        class Disk_phone
+        {
+            public bool call = true;
+            public int count_of_numbers = 10;
+
+            public virtual void Call()
+            {
+                Console.WriteLine("Hello!I can call someone, dial the number by scrolling the disk!");
+            }
+        }
+
+        class Knop_phone : Disk_phone
+        {
+            public bool Buttons = true;
+
+            public override void Call()
+            {
+                Console.WriteLine("Hello! I can call someone using buttons!");
+            }
+        }
+
+        class white_dark_phone : Knop_phone
+        {
+            public bool display = true;
+            public override void Call()
+            {
+                Console.WriteLine("Hello-hello! I can call to someone using white-dark display!");
+            }
+        }
+
+        class color_phone : white_dark_phone
+        {
+            public bool wi_fi = true;
+            public override void Call()
+            {
+                Console.WriteLine("I’m calling the one you click on.");
+            }
+
+            public virtual void Surf_Net()
+            {
+                Console.WriteLine("I can surf the internet using wi-fi");
+            }
+        }
+
+        class iPhone : color_phone
+        {
+            public override void Call()
+            {
+                Console.WriteLine("Well, put on your earpods and call to your friends");
+            }
+
+            public override void Surf_Net()
+            {
+                Console.WriteLine("We're using wi-fi and maybe 5g(:D). Do you want to watch something on Instagram?");
+            }
+        }
+
+        class Google_glass : iPhone
+        {
+            public int lenses = 2;
+            public override void Call()
+            {
+                Console.WriteLine("Say 'Ok, Gooooogle, let's call to this guys'.");
+            }
+            
+            public override void Surf_Net()
+            {
+                Console.WriteLine("You have a browser, but I don’t remember where it is!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Disk Phone");
+            Disk_phone dp = new Disk_phone();
+            dp.Call();
+            Console.WriteLine();
+
+            Console.WriteLine("Button Phone");
+            Knop_phone kp = new Knop_phone();
+            kp.Call();
+            Console.WriteLine();
+
+            Console.WriteLine("Black&White Phone");
+            white_dark_phone siemens = new white_dark_phone();
+            siemens.Call();
+            Console.WriteLine();
+
+            Console.WriteLine("Color Phone");
+            color_phone xiaomi = new color_phone();
+            xiaomi.Call();
+            xiaomi.Surf_Net();
+            Console.WriteLine();
+
+            Console.WriteLine("iPhone");
+            iPhone ten_pro_max = new iPhone();
+            ten_pro_max.Call();
+            ten_pro_max.Surf_Net();
+            Console.WriteLine();
+
+            Console.WriteLine("Google Glass");
+            Google_glass Glasses = new Google_glass();
+            Glasses.Call();
+            Glasses.Surf_Net();
+            Console.ReadKey();
+        }
+    }
+}
