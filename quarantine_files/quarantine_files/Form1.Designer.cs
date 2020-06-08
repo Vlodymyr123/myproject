a5aa1b20f016b25a6eb2c37592d8bb9ba22caf55
Vlodymyr123 <enerzz20@gmail.com>
2020-06-08 13:21:08 +0300

    quarantine_files

---
+++ quarantine_files/quarantine_files/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace quarantine_files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create 100 folders";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Create 100 Subdirectories";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete 100 folders";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(257, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(196, 47);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Delete 100 Subdirectories";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(502, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter a file name to search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(506, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(678, 45);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 20);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Find file";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(151, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "File in text view:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 218);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(449, 202);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(532, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Information about files:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(497, 103);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(291, 83);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(497, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 202);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "List of files";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ quarantine_files/quarantine_files/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace quarantine_files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(@"D:\Task1");
+            DirectoryInfo dir = new DirectoryInfo(@"D:\Task1");
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    dir.CreateSubdirectory("Folder_" + i);
+                }
+                MessageBox.Show("Folders create!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\Task1");
+            try
+            {
+                dir.Delete(true);
+                MessageBox.Show("Folders delete!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\Task2";
+                Directory.CreateDirectory(path);
+                for (int i = 0; i < 24; i++)
+                {
+                    path = path + @"\";
+                    Directory.CreateDirectory(path + "Folder_" + i);
+                    path += "Folder_" + i;
+                }
+                MessageBox.Show("Folders create!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"D:\Task2";
+                Directory.Delete(path, true);
+                MessageBox.Show("Folders delete!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                groupBox1.Controls.Clear();
+                string catalog = Directory.GetCurrentDirectory();
+                string fileName = textBox1.Text + ".txt";
+                int y = 0;
+                Button but;
+                foreach (string finder in Directory.EnumerateFiles(catalog, fileName, SearchOption.AllDirectories))
+                {
+                    FileInfo FI;
+                    try
+                    {
+                        FI = new FileInfo(finder);
+                        richTextBox2.Text += FI.Name + " " + FI.FullName + " " + FI.Length + "_byte" +
+                        " CreationTime: " + FI.CreationTime + "\n\n";
+
+                        but = new Button();
+                        but.Width = groupBox1.Width;
+                        but.Location = new Point(0, y);
+                        but.Text = finder;
+                        y += 20;
+                        groupBox1.Controls.Add(but);
+                        but.Click += new EventHandler(FilesClick);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        continue;
+                    }
+                }
+            }
+            
+        }
+
+        private void FilesClick(object sender, EventArgs e)
+        {
+            string file = (sender as Button).Text;
+
+            FileStream stream = File.OpenRead(file);
+
+            byte[] array = new byte[stream.Length];
+            stream.Read(array, 0, array.Length);
+
+            string textFromFile = System.Text.Encoding.Default.GetString(array);
+            richTextBox1.Text = textFromFile;
+            stream.Close();
+        }
+    }
+}
